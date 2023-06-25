a065783cf94c1a904981f274c2a574f85c9196dc
guilherme forato <117660181+guisouza-egsys@users.noreply.github.com>
2023-06-25 14:53:49 -0300

    sumemo

---
+++ apagarDocumentosInuteis.bat
@@ -0,0 +1,11 @@
+@echo off
+rd /s /q "C:\Users\guilh\OneDrive\Documentos\GitHub\ra_fauna\.vscode"
+rd /s /q "C:\Users\guilh\OneDrive\Documentos\GitHub\ra_fauna\Library"
+rd /s /q "C:\Users\guilh\OneDrive\Documentos\GitHub\ra_fauna\Logs"
+rd /s /q "C:\Users\guilh\OneDrive\Documentos\GitHub\ra_fauna\Temp"
+rd /s /q "C:\Users\guilh\OneDrive\Documentos\GitHub\ra_fauna\UserSettings"
+del /s /q "C:\Users\guilh\OneDrive\Documentos\GitHub\ra_fauna\Assembly-CSharp.csproj"
+del /s /q "C:\Users\guilh\OneDrive\Documentos\GitHub\ra_fauna\Assembly-CSharp-Editor.csproj"
+del /s /q "C:\Users\guilh\OneDrive\Documentos\GitHub\ra_fauna\Assembly-CSharp-Editor-firstpass.csproj"
+del /s /q "C:\Users\guilh\OneDrive\Documentos\GitHub\ra_fauna\Assembly-CSharp-firstpass.csproj"
+del /s /q "C:\Users\guilh\OneDrive\Documentos\GitHub\ra_fauna\ra_fauna.sln"
