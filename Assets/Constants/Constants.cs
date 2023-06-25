8c18f0588cd0ff48f41d532ab213d58e76935824
guilherme forato <117660181+guisouza-egsys@users.noreply.github.com>
2023-06-25 14:36:46 -0300

    commit

---
+++ Assets/Constants/Constants.cs
@@ -0,0 +1,5 @@
+public static class Constants
+{
+    public const string WARNING_PAIS = "warningPais";
+    public const string LINK_TERMS = "https://docs.google.com/document/d/1XO5lv94ksvW1rKlYdbiNeYKrcn0nBS2rIsUpGnm_ges/edit";
+}
